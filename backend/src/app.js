d10c31f598076c9ed270cbd09061fb93e9e013ec
eugide <eugidendungutse770@gmail.com>
2025-02-11 11:46:07 +0200

    My exercise Website

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.listen(port, async () => {
-    await connectDB();
+  await connectDB();
   console.log(`Server started at http://localhost:${port}`);
 });
