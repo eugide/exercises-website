9ab4863ee6b405bc9860d6663e2b20f088acddde
eugide <eugidendungutse770@gmail.com>
2025-02-17 15:17:36 +0200

    chore: setup Add the  Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Welcome from "./pages/welcome/Welcome";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
