e6d58c1c3c5137129ef9e40144784f7983d50261
eugide <eugidendungutse770@gmail.com>
2025-02-18 16:49:34 +0200

    feat: add user model

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,10 +4,18 @@ function Navbar() {
       <div className="flex justify-between px-6 py-3 bg-[#c4c2c1]">
         <h1 className="text-2xl font-bold">Alight Coding School</h1>
         <div className="flex gap-x-5">
-          <button className="font-bold px-6 py-2 border border-blue-800 
-               text-blue-800 rounded-sm hover:bg-blue-800 hover:text-white">Login</button>
-          <button className="font-bold px-6 py-2 border border-blue-800 
-             text-white bg-blue-800 hover:bg-white hover:text-blue-800 rounded-sm">Register</button>
+          <button
+            className="font-bold px-6 py-2 border border-blue-800 
+               text-blue-800 rounded-sm hover:bg-blue-800 hover:text-white"
+          >
+            Login
+          </button>
+          <button
+            className="font-bold px-6 py-2 border border-blue-800 
+             text-white bg-blue-800 hover:bg-white hover:text-blue-800 rounded-sm"
+          >
+            Register
+          </button>
         </div>
       </div>
     </>
